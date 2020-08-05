976b5d8d4829b0da6c54305c1d160c54e0d58810
Titusz Pan <titusz.pan@gmail.com>
2020-08-05 13:13:01 +0200

    Add option to set actor account.

--- iscc_registry/publish.py
+++ iscc_registry/publish.py
@@ -13,13 +13,13 @@ def get_live_contract(addr=iscc_registry.settings.contract_address, account=0):
     return w3.eth.contract(address=addr, abi=compile_registry()["abi"])
 
 
-def publish(iscc_code, cid):
+def publish(iscc_code, cid, account=0):
     components = iscc_split(iscc_code)
     iscc_raw = b"".join([iscc.decode(code) for code in components])
     log.debug(f"Raw ISCC ({len(iscc_raw)} bytes): {iscc_raw.hex()}")
     cid_raw = tools.cid_to_sha256(cid)
     log.debug(f"Raw CIDv0 ({len(cid_raw)} bytes): {cid_raw.hex()}")
-    ct = get_live_contract()
+    ct = get_live_contract(account=account)
     tx_hash_digest = ct.functions.register(iscc=iscc_raw, cid=cid_raw).transact()
     log.debug(f"ISCC registered (txid: {tx_hash_digest.hex()})")
     return tx_hash_digest.hex()
